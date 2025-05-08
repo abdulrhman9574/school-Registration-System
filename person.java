759b944c7579c457e026a846877340431fbd68fe
Ali Mohamed Saleh Ahmed <aliali84666666@gmail.com>
2025-05-08 14:38:03 +0300

    Update person.java

--- person.java
+++ person.java
@@ -1,11 +1,15 @@
-
 package com.mycompany.school;
 
-    
-    public abstract class Person {
+// Abstract class Person to be inherited by Student and Teacher
+public abstract class Person {
+
+    // Name of the person (common attribute for all people in school)
     protected String name;
+
+    // Age of the person (common attribute for all people in school)
     protected int age;
-        
+
+   
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
@@ -14,23 +18,25 @@ package com.mycompany.school;
     public String getName() {
         return name;
     }
-        
-  public void setName(String name) {
+
+
+    public void setName(String name) {
         this.name = name;
     }
-  
-     public int getAge() {
-            return age;
-        }
 
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age - allows updating the age
     public void setAge(int age) {
-            this.age = age;
-        }
+        this.age = age;
+    }
 
-
-   
+    /**
+     * Abstract method to be implemented in subclasses (e.g. Student, Teacher)
+     * Used to display specific information about the person depending on their role.
+     */
     public abstract void information();
-
 }
-
-
