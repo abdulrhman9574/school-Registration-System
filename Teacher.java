d2e12c6199867c3aa76738969b08b2ebcb972d65
mohamednour770 <mido1942006@gmail.com>
2025-05-08 21:19:57 +0300

    modified Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,66 +1,70 @@
 
-package com.mycompany.school;
+package course;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Teacher extends Person {
 //    data fields
     private double salary;
     private String department;
+    private String rank;
 //   constructor
-    public Teacher(double salary, String department, String name, int age) {
+    public Teacher(String name, int age,String rank, String department ,double salary ) {
         super(name, age);
         this.salary = salary;
         this.department = department;
+        this.rank = rank;
     }
-//     method to get salary depending on teacher's rank
+//  Setters and Getters
     public double getSalary() {
-        int choice;
-        String rank;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Choose your rank to know your salary :");
-        System.out.println("1. Teaching Assistant");
-        System.out.println("2. Teacher");
-        System.out.println("3. Senior Teacher");
-        System.out.println("4. Professor");
-        choice = sc.nextInt();
-        sc.nextLine();
-        switch (choice) {
-            case 1:
-                rank = "Teaching Assistant";
-                return 10000;
-            case 2:
-                rank = "Teacher";
-                return 15000;
-            case 3:
-                rank = "Senior Teacher";
-                return 20000;
-            case 4:
-                rank = "Professor";
-                return 25000;
-            default:
-                System.out.println("ERROR");
-                return 0;
-        }
-
+        return salary;
     }
-//  setter for salary
+
     public void setSalary(double salary) {
         this.salary = salary;
     }
-//  getter for department
+
     public String getDepartment() {
         return department;
     }
-//  setter for department
-    public void setDepartment( String department) {
+
+    public void setDepartment(String department) {
         this.department = department;
     }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    
+//    initialized an ArrayList
+      ArrayList<String> teacherList = new ArrayList<String>();
+      
+//      The list of Teachers
+      {
+       teacherList.add("Dr.Mohamed");
+       teacherList.add("Dr.Mahmoud");
+       teacherList.add("Dr.Ahmed");
+       teacherList.add("Dr.Hussein");
+       teacherList.add("Dr.Waleed"); 
+      }
+//      method to add a new Teacher
+      public void addTeacher (String Teacher) {
+          teacherList.add(Teacher);
+      }
+//      method do display the list of Teachers    
+       public void displayTeacherList() {
+           System.out.println(teacherList);
+       }
     
     @Override
     public void information(){
         System.out.println("Name: " + getName());
         System.out.println("Age: " + getAge());
+        System.out.println("Rank: "+getRank());
         System.out.println("department: " + getDepartment());
         System.out.println("Salary: " + getSalary());
     }
