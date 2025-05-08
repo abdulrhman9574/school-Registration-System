c07a45b2f86d9908d78bbe1d1823e1a165683775
User <aliali84666666@gmail.com>
2025-05-08 14:22:33 +0300

    person

---
+++ person.java
@@ -0,0 +1,48 @@
+
+package com.mycompany.school;
+
+
+public class person {
+
+    
+    public abstract class Person {
+    protected String name;
+    protected int id;
+    protected int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+  public void setName(String name) {
+        this.name = name;
+    }
+  
+     public int getAge() {
+            return age;
+        }
+
+    public void setAge(int age) {
+            this.age = age;
+        }
+
+  
+
+  
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+   
+    public abstract void information();
+
+}
+
+}
